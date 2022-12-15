51267902e163d11bda335e3595836b97a5ad23cc
alexdembele <chessdembel@gmail.com>
2022-12-15 15:22:23 +0100

    ajout classe

---
+++ inc/grille.hpp
@@ -0,0 +1,20 @@
+#ifndef INC_GRILLE_HPP
+#define INC_GRILLE_HPP
+
+#include "SFML/System/Vector2.hpp"
+#include <SFML/Graphics.hpp>
+
+const int hauteur=20;
+const int largeur=10;
+
+
+class grille
+{
+    private:
+        int field[hauteur][largeur] ;
+    public:
+
+        
+};
+
+#endif
---
+++ inc/piece.hpp
@@ -0,0 +1,37 @@
+#ifndef INC_PIECE_HPP
+#define INC_PIECE_HPP
+
+#include "SFML/System/Vector2.hpp"
+#include <SFML/Graphics.hpp>
+
+int figures[7][4] =
+{
+    1,3,5,7, // I
+    2,4,5,7, // Z
+    3,5,4,6, // S
+    3,5,4,7, // T
+    2,3,5,7, // L
+    3,5,7,6, // J
+    2,3,4,5, // O
+};
+
+struct point
+{
+    int x;
+    int y;
+};
+
+
+class Piece
+{ 
+    private:
+        point carres[4];
+
+
+    public:
+        explicit Piece(int grille);//construit une pièce aléatoirement dans la grille spécifié
+        void rotate();
+        void move();
+}
+;
+#endif
